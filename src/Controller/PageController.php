5a91a2f3be31dbbbb0c253fc351a37fe2b22779a
Corentin Watissé <corentin.watisse@etud.u-picardie.fr>
2022-11-15 21:44:02 +0100

    DataTable : Theme / Users

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -2,7 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\Themes;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
+use Omines\DataTablesBundle\Adapter\Doctrine\ORMAdapter;
+use Omines\DataTablesBundle\Column\TextColumn;
+use Omines\DataTablesBundle\Column\TwigStringColumn;
+use Omines\DataTablesBundle\DataTableFactory;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PageController extends AbstractController
@@ -10,6 +19,52 @@ class PageController extends AbstractController
     #[Route('/', name: 'homepage')]
     function homepage() {
         return $this->render('base.html.twig');
+    }
+
+
+    #[Route('/user-list', name: 'user-list')]
+    public function showAction(Request $request, DataTableFactory $dataTableFactory)
+    {
+        $table = $dataTableFactory->create()
+            ->add('Email', TextColumn::class, ['field' => 'user.email', 'label' => "Email"])
+//            ->add('Roles', ::class, ['field' => 'user.roles'])
+            ->add('Pseudo', TextColumn::class, ['field' => 'user.Pseudo', 'label' => "Pseudo"])
+            ->add('Nom', TextColumn::class, ['field' => 'user.Nom', 'label' => "Nom"])
+            ->add('Prenom', TextColumn::class, ['field' => 'user.Prenom', 'label' => "Prenom"])
+            ->add('is_verified', TextColumn::class, ['field' => 'user.isVerified', 'label' => 'Is verified ?'])
+            ->add('link', TwigStringColumn::class, ['label' => 'Delete', 'template' => "<a href='/delete_user/{{ row.id }}'> <i class='bx bx-message-alt-x'></i> </a>"])
+
+
+            ->createAdapter(ORMAdapter::class, [
+                'entity' => User::class,
+            ])
+            ->handleRequest($request);
+
+        if ($table->isCallback()) {
+            return $table->getResponse();
+        }
+
+        return $this->render('user-list.html.twig', ['datatable' => $table]);
+    }
+
+
+    #[Route('/delete_user/{id}', name: 'delete_user')]
+    function deleteTheme($id, EntityManagerInterface $entityManager, ManagerRegistry $doctrine){
+
+        $entityManager = $doctrine->getManager();
+        $product = $entityManager->getRepository(User::class)->find($id);
+
+        if (!$product) {
+            throw $this->createNotFoundException(
+                'No user found for id '.$id
+            );
+        }
+
+        $entityManager->remove($product);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('user-list');
+
     }
 
 }
--- src/Controller/ThemeController.php
+++ src/Controller/ThemeController.php
@@ -5,6 +5,11 @@ namespace App\Controller;
 use App\Entity\Themes;
 use App\Form\NewThemeFormType;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
+use Omines\DataTablesBundle\Adapter\Doctrine\ORMAdapter;
+use Omines\DataTablesBundle\Column\TextColumn;
+use Omines\DataTablesBundle\Column\TwigStringColumn;
+use Omines\DataTablesBundle\DataTableFactory;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,7 +18,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class ThemeController extends AbstractController
 {
     #[Route('/new_theme', name: 'new_theme')]
-    public function register(Request $request,  EntityManagerInterface $entityManager): Response
+    public function register(Request $request,  EntityManagerInterface $entityManager, DataTableFactory $dataTableFactory): Response
     {
         $theme = new Themes();
 
@@ -27,8 +32,44 @@ class ThemeController extends AbstractController
             $entityManager->persist($theme);
             $entityManager->flush();
         }
+
+
+        $table = $dataTableFactory->create()
+            ->add('id', TextColumn::class, ['field' => 'themes.id', 'label' => "Theme's ID"])
+            ->add('nom', TextColumn::class, ['field' => 'themes.Nom', 'label' => "Theme's name"])
+            ->add('link', TwigStringColumn::class, ['label' => 'Delete', 'template' => "<a href='/delete_theme/{{ row.id }}'> <i class='bx bx-message-alt-x'></i> </a>"])
+            ->createAdapter(ORMAdapter::class, [
+                'entity' => Themes::class,
+            ])
+            ->handleRequest($request);
+
+        if ($table->isCallback()) {
+            return $table->getResponse();
+        }
+
+
         return $this->render('forms/newTheme.html.twig', [
             'NewThemeForm' => $form->createView(),
+            'datatable' => $table,
         ]);
+    }
+
+    #[Route('/delete_theme/{id}', name: 'delete_theme')]
+    function deleteTheme($id, EntityManagerInterface $entityManager, ManagerRegistry $doctrine){
+
+        $entityManager = $doctrine->getManager();
+        $product = $entityManager->getRepository(Themes::class)->find($id);
+
+        if (!$product) {
+            throw $this->createNotFoundException(
+                'No theme found for id '.$id
+            );
+        }
+
+        $entityManager->remove($product);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('new_theme');
+
     }
 }
